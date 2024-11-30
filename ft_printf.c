fe75ee036f3c47d77fe88425cc5261af933765cc
ggamzed <gamzeddmrc@gmail.com>
2024-11-30 19:03:44 +0300

    ft_printf.c norm ok

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,4 @@
 #include "ft_printf.h"
-#include <stdarg.h>
 #include <unistd.h>
 
 static int	ft_print_char(int c)
@@ -9,71 +8,57 @@ static int	ft_print_char(int c)
 
 int	ft_formats(va_list args, const char format)
 {
-	int		printed_len;
-
-	printed_len = 0;
 	if (format == 'c')
-		printed_len += ft_print_char(va_arg(args, int)); // neden int ?
-	/*
-	In function ‘print_numbers’:
-den.c:10:22: warning: ‘char’ is promoted to ‘int’ when passed through ‘...’
-   10 |     a = va_arg(args, char); // char yapınca böyle warning veriyor
-	*/
-	/*
-	C dilinde, değişken sayıda argümanlarla çalışırken, va_arg makrosu parametrelerin tam türünü bilmek zorundadır. 
-	Fakat C dilinde, char türündeki bir argüman, int türüne genişletilir (promote edilir) fonksiyon çağrısı sırasında.
-	Genişletme (promotion), değişken sayıda parametre alan fonksiyonlar için gereklidir çünkü:
-
-	Derleyici, küçük türlerin taşınmasını (örneğin, char ve short türlerini) ve işlenmesini daha verimli hale getirebilir.
-	Değişken sayıda parametreli fonksiyonlarda, parametrelerin türü belirli olmayabilir, bu nedenle küçük türler (örneğin char) 
-	daha geniş türlere (int) genişletilir.
-	void func(char a, ...) // peki buradaki a karakteri genişler mi?
-	lk parametre (yani a) genişlemez, çünkü değişken sayıda parametrelerin türlerinin 
-	genişletilmesi sadece ... kısmındaki parametreler için geçerlidir.
-	bellek hizalaması
-	*/
+		return (ft_print_char(va_arg(args, int)));
 	else if (format == 's')
-		printed_len += ft_print_string(va_arg(args, char *));
+		return (ft_print_string(va_arg(args, char *)));
 	else if (format == 'd' || format == 'i')
-		printed_len += ft_print_int(va_arg(args, int));
+		return (ft_print_int(va_arg(args, int)));
 	else if (format == 'x' || format == 'X')
-		printed_len += ft_print_hex(va_arg(args, unsigned int), format);
+		return (ft_print_hex(va_arg(args, unsigned int), format));
 	else if (format == 'p')
-		printed_len += ft_print_point(va_arg(args, unsigned long), format);
-	else if(format == 'u')
-		printed_len += ft_print_unsigned(va_arg(args, unsigned int));
+		return (ft_print_point(va_arg(args, unsigned long), format));
+	else if (format == 'u')
+		return (ft_print_unsigned(va_arg(args, unsigned int)));
 	else if (format == '%')
-		printed_len += ft_print_char('%');
-	return (printed_len);
+		return (ft_print_char('%'));
+	else
+		return (0);
+}
+
+int	ft_check(char str)
+{
+	if (str == 'c' || str == 'd' || str == 'i' || str == 'u' || str == '%'
+		|| str == 's' || str == 'x' || str == 'X' || str == 'p')
+		return (1);
+	return (0);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	unsigned int	i;
-	va_list			args;
-	int				printed_len;
-	if(str == NULL)
-		return (-1);
-	i = 0;
+	va_list	args;
+	int		printed_len;
+	int		tmp;
+
 	printed_len = 0;
 	va_start(args, str);
-	while(str[i])
+	while (*str)
 	{
-		if(str[i] == '%')
+		if (*str == '%' && ft_check(*(++str)))
 		{
-			printed_len += ft_formats(args, str[i + 1]);
-			i++;
+			tmp = ft_formats(args, *(str++));
+			if (tmp == -1)
+				return (-1);
+			printed_len += tmp;
 		}
-		else
-			printed_len += ft_print_char(str[i]);
-		i++;
+		else if (*str != '%')
+		{
+			tmp = ft_print_char(*(str++));
+			if (tmp == -1)
+				return (-1);
+			printed_len += tmp;
+		}
 	}
 	va_end(args);
 	return (printed_len);
-}
-#include <stdio.h>
-int	main()
-{
-	int i = -2;
-	ft_printf("%d", i);
 }
